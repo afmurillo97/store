3808a0615a7f57b18a7d08a67877dd5148f8d510
afmurillo97 <afmurillo97@gmail.com>
2023-01-24 13:32:33 -0500

    add colors and screen sizes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'red' : '#ff2929',
+        'red-hover' : '#ff6666'
+      },
+      screens: { // Tailwind is Mobile First!!
+        'tablet-mini': '640px',
+        // => @media (min-width: 640px) { ... }
+        'tablet': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
   },
   plugins: [],
 }
