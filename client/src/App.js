212f216ffdfd705b60186830da077986c3ca25c9
afmurillo97 <afmurillo97@gmail.com>
2023-01-24 13:33:09 -0500

    make imports and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,43 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
+  Outlet,
 } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
+import Product from './pages/Product/Product';
+
+const Layout = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element:<span>Home</span>
+    element:<Layout/>,
+    children:[
+      {
+        path:'/',
+        element: <Home/>
+      },
+      {
+        path:'/products/:id',
+        element: <Products/>
+      },
+      {
+        path:'/product/:id',
+        element: <Product/>
+      },
+    ]
   },
-  {
-    path:'/products/:id',
-    element:<span>Category</span>
-  },
-  {
-    path:'/product/:id',
-    element:<span>Product</span>
-  }
 ])
 
 function App() {
